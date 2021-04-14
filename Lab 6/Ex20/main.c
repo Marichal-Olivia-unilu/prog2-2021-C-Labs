684ae99856551fd685223b5f7f448ea904118bbb
Olivia Marichal <olivia.marichal.001@student.uni.lu>
2021-04-14 16:46:24 +0200

    first point

---
+++ Lab 6/Ex20/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(){
+
+    char *str1 = "Olivia";
+    char str[] = "Olivia";
+    char *str2 = "Olivia";
+    str1 = "Marichal";
+
+    printf("%s\n", str1);
+    
+    printf("%s\n", str);
+
+    printf("%s\n", str2);
+
+
+
+    return 0;
+}
